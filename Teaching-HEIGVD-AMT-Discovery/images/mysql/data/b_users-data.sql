2f3ade3632ee7f5a6d89fe82702e2b24e3e2118a
dardanSelimi <dardan.selimi@heig-vd.ch>
2016-10-23 23:49:39 +0200

    Update b_users-data.sql

--- Teaching-HEIGVD-AMT-Discovery/images/mysql/data/b_users-data.sql
+++ Teaching-HEIGVD-AMT-Discovery/images/mysql/data/b_users-data.sql
@@ -7,10 +7,10 @@ USE users;
 
 SET AUTOCOMMIT=0;
 
-INSERT INTO user VALUES (NULL, "admin", "admin", "admin@admin.ch", "admin"), 
-						(NULL, "test1", "test1", "test1@test1.ch", "test"), 
-						(NULL, "test1", "test1", "test2@test2.ch", "test"),
-						(NULL, "test1", "test1", "test3@test3.ch", "test");
+INSERT INTO user VALUES (NULL, "admin", "admin", "admin@admin.ch", "admin1234"), 
+						(NULL, "test1", "test1", "test1@test1.ch", "test12345"), 
+						(NULL, "test1", "test1", "test2@test2.ch", "test12345"),
+						(NULL, "test1", "test1", "test3@test3.ch", "test12345");
 COMMIT;
 
 SET AUTOCOMMIT=1;
